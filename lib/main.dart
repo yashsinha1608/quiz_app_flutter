a32147009ac8f16afaa311b67b81d2a18ca20fe4
YASH <yash1608.sinha@gmail.com>
2025-09-18 00:26:50 +0530

    intial

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(const MaterialApp(home: 
+  Scaffold(body: 
+  StartScreen()
+  )
+  )
+  );
 }
